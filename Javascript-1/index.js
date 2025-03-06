939fbe1b7cfe379e23ca2eb70f5498d5332bb579
Sweta Patel <sweta0442@gmail.com>
2025-03-06 22:50:48 +0530

    hoisting part2

--- Javascript-1/index.js
+++ Javascript-1/index.js
@@ -1,14 +1,18 @@
-getName();
-console.log(x);
-console.log(getName);
+// getName();
+// console.log(x);
+// console.log(getName);
 
-var x = 7;
-// this will behave like a function so it will not undefined
-function getName2() {
+// var x = 7;
+// // this will behave like a function so it will not undefined
+// function getName2() {
 
-}
+// }
+// // here getName will behave like a variable so it will be undefined.
+// var getName = () => {
+//     console.log("Namaste Javascript");
+// }
 
-// here getName will behave like a variable so it will be undefined.
-var getName = () => {
-    console.log("Namaste Javascript");
-}
+// // here also behave like a variable
+// var getName3 = () => {
+//     console.log("Namaste ji");
+// }
